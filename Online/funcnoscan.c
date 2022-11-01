b0d775fc62f30876168a2b7a5fa28394aeb8c6e1
Emmanuel Joseph <emmjstud@gmail.com>
2022-11-01 17:18:34 +0000

    function with param

---
+++ Online/funcnoscan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ /*Declaring Func with param*/
+int myFunction(int x)
+{
+	  return 5 + x;
+}
+
+int main() 
+{
+	  printf("Result is: %d\n", myFunction(3));
+
+	    return 0;
+}
